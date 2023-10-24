80ab4e5773169134132a8b337077e1152fdb016c
EvanDayton <132493311+EvanDayton@users.noreply.github.com>
2023-10-24 17:29:08 -0600

    Delete Melodiq/program/Blurry background Test directory

--- Melodiq/program/Blurry background Test/script.js
+++
@@ -1,4 +0,0 @@
-import { ShaderArt } from 'https://cdn.skypack.dev/shader-art';
-import { UniformPlugin } from 'https://cdn.skypack.dev/@shader-art/plugin-uniform';
-
-ShaderArt.register([() => new UniformPlugin()]);
